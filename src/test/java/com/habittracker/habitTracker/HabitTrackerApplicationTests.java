21b2ef1c60b875c4c6c99ccb7e6b61c32ea55fbe
sannidhi shetty <sannidhishetty9@gmail.com>
2025-11-09 22:09:57 -0500

    Backend initial setup

---
+++ src/test/java/com/habittracker/habitTracker/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habittracker.habitTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
